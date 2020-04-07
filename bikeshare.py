3865304321883d6ae6a13f09fe4113d4936c645d
Mirco-Miglietta <migliettamirco@gmail.com>
2020-04-07 18:22:12 +0200

    Change the first commit
    I change the firs commit of the file from refactoring to definition of
    .csv to make more easy the compreation for the first part of the script
    
    There is not problem

--- bikeshare.py
+++ bikeshare.py
@@ -1,7 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
-#refactoring
+# definition of .csv file
 CITY_DATA = CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
               'washington': 'washington.csv' }
